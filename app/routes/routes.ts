8279b391d0028dc3b6243d6ae58b50e6f256c3b4
gitflo1 <14075033+gitflo1@users.noreply.github.com>
2018-10-03 20:39:36 +0200

    add settingscontroller to routes

--- app/routes/routes.ts
+++ app/routes/routes.ts
@@ -1,13 +1,16 @@
-import {Request, Response} from "express";
+import { SettingsController } from './../controllers/settingsController';
+import { Request, Response } from "express";
 import express from 'express';
 
 export class Routes {
+    public settingsController: SettingsController = new SettingsController();
+
     public routes(app: express.Application): void {
         app.route('/').get((req: Request, res: Response) => {
             res.status(200).send({
                 message: 'Hello from base route.'
             })
         })
-        
+        app.route('/settings').post(this.settingsController.addNewSettings);
     }
 }
