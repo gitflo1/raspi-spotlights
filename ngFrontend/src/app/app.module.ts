20b96a3ab1f5e928882256baa98af3126590b799
gitflo1 <14075033+gitflo1@users.noreply.github.com>
2018-10-06 11:55:31 +0200

    Add Angular Material imports and add first card-component

--- ngFrontend/src/app/app.module.ts
+++ ngFrontend/src/app/app.module.ts
@@ -1,14 +1,20 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
+import { MatCardModule } from '@angular/material/card';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { SettingComponent } from './setting/setting.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SettingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
